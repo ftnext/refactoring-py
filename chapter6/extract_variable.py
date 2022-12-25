42d91f3690be3b90c2448458b21de529714dedfc
ftnext <takuyafjp+develop@gmail.com>
2022-12-26 00:01:06 +0900

    [refactor] Replace with variable

--- chapter6/extract_variable.py
+++ chapter6/extract_variable.py
@@ -8,7 +8,7 @@ def price(order):
     return (
         base_price
         - max(0, order.quantity - 500) * order.item_price * 0.05
-        + min(order.quantity * order.item_price * 0.1, 100)
+        + min(base_price * 0.1, 100)
     )
 
 
