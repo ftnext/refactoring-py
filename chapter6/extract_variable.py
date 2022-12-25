02a63ae5129b905f5b54e24f24d616f675bef6ba
ftnext <takuyafjp+develop@gmail.com>
2022-12-25 23:45:03 +0900

    [test] Normal case

--- chapter6/extract_variable.py
+++ chapter6/extract_variable.py
@@ -1,3 +1,7 @@
+import unittest
+from typing import NamedTuple
+
+
 def price(order):
     # price = base price - quantity discount + shipping
     return (
@@ -5,3 +9,18 @@ def price(order):
         - max(0, order.quantity - 500) * order.item_price * 0.05
         + min(order.quantity * order.item_price * 0.1, 100)
     )
+
+
+class PriceTestCase(unittest.TestCase):
+    class Order(NamedTuple):
+        quantity: int
+        item_price: int
+
+    def test_no_discount_normal_shipping(self):
+        actual = price(self.Order(quantity=100, item_price=400))
+
+        self.assertEqual(actual, 40100)
+
+
+if __name__ == "__main__":
+    unittest.main()
