eebd77f588d3a6064047fe3c59475842425920ce
ftnext <takuyafjp+develop@gmail.com>
2022-12-26 00:29:03 +0900

    [feat] Class version before

--- chapter6/extract_variable.py
+++ chapter6/extract_variable.py
@@ -31,5 +31,26 @@ class PriceTestCase(unittest.TestCase):
                 self.assertEqual(actual, expected)
 
 
+class Order:
+    def __init__(self, a_record) -> None:
+        self._data = a_record
+
+    @property
+    def quantity(self):
+        return self._data.quantity
+
+    @property
+    def item_price(self):
+        return self._data.item_price
+
+    @property
+    def price(self):
+        return (
+            self.quantity * self.item_price
+            - max(0, self.quantity - 500) * self.item_price * 0.05
+            + min(self.quantity * self.item_price * 0.1, 100)
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
