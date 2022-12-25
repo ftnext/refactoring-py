389bbca469d855badea31230027d6f30a586124c
ftnext <takuyafjp+develop@gmail.com>
2022-12-26 00:00:20 +0900

    [refactor] Extract Variable with VS Code

--- chapter6/extract_variable.py
+++ chapter6/extract_variable.py
@@ -4,8 +4,9 @@ from typing import NamedTuple
 
 def price(order):
     # price = base price - quantity discount + shipping
+    base_price = order.quantity * order.item_price
     return (
-        order.quantity * order.item_price
+        base_price
         - max(0, order.quantity - 500) * order.item_price * 0.05
         + min(order.quantity * order.item_price * 0.1, 100)
     )
